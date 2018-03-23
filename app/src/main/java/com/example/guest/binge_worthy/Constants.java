80efbea1f01c81c8825a15c7e03f18975eaf04c0
Aaron Ross <ashmortar@gmail.com>
2018-03-23 11:22:50 -0700

    api change to recommendation and youtube service

--- app/src/main/java/com/example/guest/binge_worthy/Constants.java
+++ app/src/main/java/com/example/guest/binge_worthy/Constants.java
@@ -4,10 +4,10 @@ package com.example.guest.binge_worthy;
 
 public class Constants {
     //make api key available
-    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String MOVIE_DB_API_KEY = BuildConfig.MOVIE_DB_API_KEY;
+    public static final String TASTEDIVE_API_KEY = BuildConfig.TASTEDIVE_DB_API_KEY;
     //query parameters
     public static final String API_KEY_PARAMETER = "api_key";
-    public static final String LANGUAGE_PARAMETER = "language";
     public static final String QUERY_PARAMETER = "query";
     public static final String ADULT_CONTENT_PARAMETER = "include_adult";
     public static final String RELEASE_DATE_PARAMETER = "year";
@@ -17,7 +17,6 @@ public class Constants {
     public static final String TV_SEARCH_URL = "https://api.themoviedb.org/3/search/tv";
     public static final String PERSON_SEARCH_URL = "https://api.themoviedb.org/3/search/person";
     public static final String MULTI_SEARCH_URL = "https://api.themoviedb.org/3/search/multi";
-    public static final String GET_MOVIE_DETAIL_URL = "https://api.themoviedb.org/3/movie/";
     public static final String RATING_SEARCH_URL = "https://api.themoviedb.org/3/movie/top_rated";
     public static final String MOVIE_DETAIL_URL = "https://api.themoviedb.org/3/movie/";
 }
